7392064b7d58cd56073e30dea77a64924a436fb2
Rathna Ganjigunta <rathna.ganjigunta@gmail.com>
2020-01-27 14:27:57 -0800

    fixed knex connection string

--- server.js
+++ server.js
@@ -10,10 +10,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1', //home
-    user : 'home',
-    password : '',
-    database : 'smart-brain'
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
